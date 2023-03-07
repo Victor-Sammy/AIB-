09b2d416860a5951387b475804059dc210a19bd6
Faruq Bakare <bakarefaruq.a@gmail.com>
2023-03-07 18:16:03 +0100

    api changes

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -11,20 +11,7 @@ axios.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-export const getCart = () => {
-  const cartID = localStorage.getItem("cartID");
-  const accessToken = localStorage.getItem("USER_ACCESS_TOKEN");
 
-  try {
-    if (cartID && accessToken) {
-      return axios.get(`/ad/carts/${cartID}/items`);
-    } else {
-      return null;
-    }
-  } catch {
-    return null;
-  }
-};
 
 export const getNotifications = () => {
   try {
